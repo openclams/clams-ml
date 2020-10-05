14d7d50fa3a4936e74a240f2c31dff872889fd9b
RobinLaidig <enterprise-e@gmx.de>
2020-10-05 11:42:53 +0200

    Added exports that are required by the web-app.

--- projects/clams-ml/src/public-api.ts
+++ projects/clams-ml/src/public-api.ts
@@ -1,12 +1,37 @@
 /*
  * Public API Surface of clams-ml
  */
-import ModelFactory from './lib/factories/model-factory';
-export {ModelFactory};
 import Model from './lib/model/model';
 export {Model};
+import ModelFactory from './lib/factories/model-factory';
+export {ModelFactory};
 import JsonModel from './lib/schema/json-model';
 export {JsonModel};
 
+import Graph from './lib/model/graphs/graph';
+export {Graph};
+import Edge from './lib/model/graphs/edge';
+export {Edge};
+import Node from './lib/model/graphs/node';
+export {Node};
+
+import Element from './lib/model/graphs/sequence-diagram/element';
+export {Element};
+import Instance from './lib/model/graphs/sequence-diagram/instance';
+export {Instance};
+import Message from './lib/model/graphs/sequence-diagram/message';
+export {Message};
+import Template from './lib/model/graphs/sequence-diagram/template';
+export {Template};
+
+import State from './lib/model/graphs/user-profile/state';
+export {State};
+import Dot from './lib/model/graphs/user-profile/dot';
+export {Dot};
+import Arrow from './lib/model/graphs/user-profile/arrow';
+export {Arrow};
+
+import Component from './lib/model/service-catalog/component';
+export {Component};
 
 
