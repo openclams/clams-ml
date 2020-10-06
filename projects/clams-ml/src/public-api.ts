8a3ff85c11d980811d15c90cf1bedf5333773fde
RobinLaidig <enterprise-e@gmx.de>
2020-10-06 12:01:45 +0200

    Added further exports and improved structure

--- projects/clams-ml/src/public-api.ts
+++ projects/clams-ml/src/public-api.ts
@@ -2,36 +2,40 @@
  * Public API Surface of clams-ml
  */
 import Model from './lib/model/model';
-export {Model};
-import ModelFactory from './lib/factories/model-factory';
-export {ModelFactory};
+import ClamsProject from './lib/model/model';
 import JsonModel from './lib/schema/json-model';
-export {JsonModel};
+import JsonClamsProject from './lib/schema/json-model';
+import JsonCloudProvider from './lib/schema/service-catalog/json-cloud-provider';
+export {Model, ClamsProject, JsonModel, JsonClamsProject, JsonCloudProvider};
+
+import ModelFactory from './lib/factories/model-factory';
+import ComponentFactory from './lib/factories/service-catalogs/component-factory';
+export {ModelFactory, ComponentFactory};
 
 import Graph from './lib/model/graphs/graph';
-export {Graph};
 import Edge from './lib/model/graphs/edge';
-export {Edge};
 import Node from './lib/model/graphs/node';
-export {Node};
+import Geometry from './lib/model/graphs/geometry';
+export {Graph, Edge, Node, Geometry};
 
+import SequenceDiagram from './lib/model/graphs/sequence-diagram/sequence-diagram';
 import Element from './lib/model/graphs/sequence-diagram/element';
-export {Element};
 import Instance from './lib/model/graphs/sequence-diagram/instance';
-export {Instance};
 import Message from './lib/model/graphs/sequence-diagram/message';
-export {Message};
 import Template from './lib/model/graphs/sequence-diagram/template';
-export {Template};
+export {SequenceDiagram, Element, Instance, Message, Template};
 
 import State from './lib/model/graphs/user-profile/state';
-export {State};
 import Dot from './lib/model/graphs/user-profile/dot';
-export {Dot};
 import Arrow from './lib/model/graphs/user-profile/arrow';
-export {Arrow};
+export {State, Dot, Arrow};
 
 import Component from './lib/model/service-catalog/component';
-export {Component};
+import ComponentWrapper from './lib/model/service-catalog/component-wrapper';
+import TemplateType from './lib/model/service-catalog/template';
+import EdgeType from './lib/model/service-catalog/edge-type';
+import Pattern from './lib/model/service-catalog/pattern';
+import Service from './lib/model/service-catalog/service';
+export {Component, ComponentWrapper, TemplateType, EdgeType, Pattern, Service};
 
 
